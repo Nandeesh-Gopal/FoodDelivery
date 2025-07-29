689bb6c0e54f7a1dc3e914c381a6f684a2c5b664
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2025-07-29 22:03:56 +0530

    validation of the form is implemented

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const path=require("path")
 const mongo=require("mongoose")
 const app=express()
 mongo.connect("mongodb+srv://nandeesh:Nandbtech07@cluster0.ko2qcxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("Mongo connected"))
-const User=mongo.model("user",{email:String,password:String});
+const User=mongo.model("user",{name:String,email:String,password:String});
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname)))
 app.get("/",(req,res)=>{
@@ -11,11 +11,12 @@ app.get("/",(req,res)=>{
 })
 app.post("/signup",(req, res) => {
         const user = new User({
+            name:req.body.name,
             email: req.body.email,
             password: req.body.password
         });
         user.save();
-        res.send("User successfully registered!");
+    res.sendFile(path.join(__dirname,"index.html"))
 });
 app.listen(3000,()=>{
     console.log("server is running")
