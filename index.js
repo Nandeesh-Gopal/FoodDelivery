1887b95079fe708d286eb7d53c3b93cf1cc39001
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2025-07-31 20:04:13 +0530

    some changes made

--- index.js
+++
@@ -1,5 +0,0 @@
-console.log("script is running")
-const params =new URLSearchParams(window.location.search)
-const messages=params.get("message")
-if(messages){
-document.getElementById("message").innerHTML=messages;}
--- login.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(window.location.search)
-const message= params.get("message")
-if(message){
-document.getElementById("mess").innerHTML=message}
